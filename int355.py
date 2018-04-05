976495a7da8a03918c3429fed0a46c585ec04cda
Matthew <avalanchian@gmail.com>
2018-04-05 21:53:40 +0100

    Still buggy. Added Cupboard class.

--- int355.py
+++ int355.py
@@ -16,20 +16,32 @@ class Recipe:
         return int(min(limits))
 
     def make(self, num, available):
+        """Reduces the available ingredients by the amount required (num)."""
         for i in range(len(self.ingredients)):
-            available[i] =- (num * self.ingredients[i])
+            available[i] -= (num * self.ingredients[i])
+            
+
+class Cupboard:
+    def __init__(self, contents):
+        self.contents = contents
 
 
 pumpkin = Recipe("pumpkin", [1, 0, 3, 4, 3])
 apple = Recipe("apple", [0, 1, 4, 3, 2])
-cupboard = [10, 14, 10, 42, 24]
+cupboard = Cupboard([12,4,40,30,40])
+working = Cupboard([12,4,40,30,40])
 made = []
 
-for i in range(pumpkin.max_poss(cupboard)):
-    working = cupboard
-    pumpkin.make(i, working)
-    made.append([i, apple.max_poss(working)])
+
+for i in range((pumpkin.max_poss(cupboard.contents)) + 1):
+    print(cupboard.contents)# Debugging.
+    pumpkin.make(i, working.contents)
+    print(cupboard.contents) # Debugging.
+    made.append([i, apple.max_poss(working.contents)])
     made[i].append(sum(made[i]))
+    working = Cupboard([12,4,40,30,40])
+
+print(made) # Debugging.
 
 maximums = [i[2] for i in made]
 max_pies = max(maximums)
